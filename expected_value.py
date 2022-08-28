146c79169a41f57a0a09d46a1f2f6c36c749d807
Eric Pacuit <epacuit@umd.edu>
2022-08-28 18:03:10 -0400

    styling update

--- expected_value.py
+++ expected_value.py
@@ -15,16 +15,16 @@ col1, col2 = st.columns(2)
 with col1: 
     prize1 = st.number_input(label="prize 1 ", value=100)
 with col2:
-    p1 = st.number_input(label="proability 1 ", min_value=0.0, max_value=1.0, value=0.5)
+    p1 = round(st.number_input(label="proability 1 ", min_value=0.0, max_value=1.0, value=0.5), ndigits=2)
 
 col1, col2 = st.columns(2)
 with col1: 
-    prize2 = st.number_input(label="prize 2 ", value=0)
+    prize2 = st.number_input(label="prize 2 ", value=0) 
 with col2:
-    p2 = st.number_input(label="proability 2 ", min_value=0.0, max_value=1.0, value=0.5)
+    p2 = round(st.number_input(label="proability 2 ", min_value=0.0, max_value=1.0, value=0.5), ndigits=2)
 
 if p1 + p2 != 1.0:
-    st.error(f"The sum of the probabilities must be 1: {p1} + {p2} = {p1 + p2}")
+    st.error(f"The sum of the probabilities must be 1: {p1} + {p2} = {round(p1 + p2, ndigits=2)}")
 
 else: 
     L = [(prize1, p1), (prize2, p2)]
@@ -41,9 +41,7 @@ else:
         else: 
             payouts.append(prize2)
 
-    st.write(f"""After playing  the lottery {num_trials} times: """)
-    st.write(f"""The average payout is {np.average(payouts)}""")
-    st.write(f"""The bar graph gives the number of times the each prize was won.""")
+    st.write(f"""After playing  the lottery {num_trials} times, the average payout is **{np.average(payouts)}**. The bar graph gives the number of times the each prize was won.""")
     fig = px.bar(x=["$"+str(prize1),  "$"+str(prize2)], y=[len([1 for p in payouts if p == prize1]), len([1 for p in payouts if p == prize2])], labels={
                      "x": "prize",
                      "y": "number of times",
